3cbab00d9417e2057e9369fcf9f08f9d7856d148
Andres Gutierrez <anfegumos2005@gmail.com>
2026-07-18 00:50:10 -0500

    se modifica llm.py, dependencias no encontradas

--- llm.py
+++ llm.py
@@ -1,6 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from my_keys import GEMINI_API_KEY
-from my_models import GEMINI_FLASH
+import streamlit as st
+#from my_keys import GEMINI_API_KEY
+#from my_models import GEMINI_FLASH
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+GEMINI_FLASH = st.secrets["GEMINI_FLASH"]
 
 llm = ChatGoogleGenerativeAI(
     model=GEMINI_FLASH,
